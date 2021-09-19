e39af7bb71e7e911d84c8be236262338285a8564
Bhagyarekha <bhagyarekha.deshpande@gmail.com>
2021-09-19 19:54:42 +0530

    added env file

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ app.use('/contacts', contactRoutes);
 //app.use(bodyParser.json({limit:"25mb", extended:true}));
 //app.use(bodyParser.urlencoded({limit:"25mb", extended:false}));
 
-const CONNECTION_URL = "mongodb+srv://bhagyadeshpande:managecontacts@cluster0.rhlxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL, {
+mongoose.connect(process.env.CONNECTION_URL, {
   useNewUrlParser : true, useUnifiedTopology:true
 }).then(() => app.listen(PORT, () =>
 console.log(`connectection established and running on port:${PORT}`)
